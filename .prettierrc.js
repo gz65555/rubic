9c62bede510e7f65dbfce216c4312fba591b519d
gz65555 <gz65555@126.com>
2019-11-09 15:28:06 +0800

    chore: change tslint to eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports =  {
   "printWidth": 120,
-  "semi": false,
-  "singleQuote": true,
+  "semi": true,
+  "singleQuote": false,
   "trailingComma": "all",
   "bracketSpacing": true,
   "jsxBracketSameLine": true,
